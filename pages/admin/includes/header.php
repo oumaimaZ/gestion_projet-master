8bc5b8664322c07f8668a214711e1cecbdeeecf6
Amine Elouattar <elouattar@gmail.com>
2016-06-02 23:31:51 +0100

    token csss found

--- pages/admin/includes/header.php
+++ pages/admin/includes/header.php
@@ -38,6 +38,9 @@
     <!-- Custom Fonts -->
     <link href="../../bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
+    <!-- Custom Fonts -->
+    <link href="../../dist/css/bootstrap-tokenfield.css" rel="stylesheet" type="text/css">
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
